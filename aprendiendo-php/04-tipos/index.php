eac4bd00fa0b6377139be241f5793230d478f431
Arielgmv <ariel.munoz.villegas@gmail.com>
2018-12-21 15:34:50 -0400

    Avances_(21-12-18)

---
+++ aprendiendo-php/04-tipos/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ Tipos de datos 
+ * entero (int / integer) = 99
+ * coma flotante / decimales (float / double) = 3.45
+ * cadenas (string) = 'hola yo soy un string'
+ * boleano (bool) = true false
+ * null
+ * array (colección de datos)
+ * objetos
+ */
+$numero=100;
+$decimal=27.9;
+$cadena='PHP';
+$verdadero=TRUE;
+echo gettype($numero).'<br />';
+echo gettype($decimal).'<br />';
+echo gettype($cadena);
+?>
