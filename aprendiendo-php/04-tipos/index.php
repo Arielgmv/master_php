b0c6c8d13a2fcacfe7256a745b65a1c7d817c306
Arielgmv <ariel.munoz.villegas@gmail.com>
2018-12-26 14:35:26 -0400

    Avances_(26-12-18)

--- aprendiendo-php/04-tipos/index.php
+++ aprendiendo-php/04-tipos/index.php
@@ -11,9 +11,31 @@
  */
 $numero=100;
 $decimal=27.9;
+$texto='Soy un texto $';
+/*debemos utilizar comilla doble para que muestre el valor de la
+variable número*/
+$texto_1="Soy un texto \n $numero";
+/*esto es más rapido que con comilla doble, con comilla doble
+PHP está esperando una variable para imprimimir su valor, tarda un
+poco más a nivel de procesador*/
+$texto_2='Soy un texto '.$numero;
 $cadena='PHP';
-$verdadero=TRUE;
+$verdadero=false;
+$nula=null;
 echo gettype($numero).'<br />';
+echo gettype($texto).'<br />';
 echo gettype($decimal).'<br />';
-echo gettype($cadena);
+echo gettype($cadena).'<br />';
+echo gettype($verdadero).'<br />';
+echo gettype($nula).'<br />';
+echo $texto.'<br />';
+echo $texto_1.'<br />';
+echo $texto_2.'<br />';
+//Debugear (ver que tiene dentro)
+$mi_nombre='Ariel Muñoz Atux Empleos';
+var_dump($mi_nombre);
+echo '<br />';
+$mi_array[]='prueba 1';
+$mi_array[]='prueba de 2';
+var_dump($mi_array);
 ?>
