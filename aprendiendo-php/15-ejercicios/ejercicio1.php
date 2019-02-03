89ccf37822364e0f910edcf255bedfe5aff44bad
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-02-03 17:07:45 -0400

    Avances_(3-2-19)

---
+++ aprendiendo-php/15-ejercicios/ejercicio1.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * Ejercicio1. Hacer un programa que tenga un array con 8 números enteros
+ * que haga lo siguiente:
+ * Recorrerlo y mostrarlo
+ * Ordenarlo y mostrarlo
+ * Mostrar su longitud
+ * buscar algún elemento 
+ */
+function mostrarArray($numeros){
+    $resultado='';    
+    foreach($numeros as $numero){
+        //$resultado=$resultado.$numero.''<br/>;
+        $resultado.= $numero.'<br/>';        
+    }
+    return $resultado;
+}
+
+echo 'Recorrer y mostrar <br/>';
+$numeros=array(5, 8, 65, 24, 1, 0, 6, 9);
+echo mostrarArray($numeros);
+echo '<hr/>';
+
+echo 'Ordenar y mostrar <br/>';
+sort($numeros);
+echo mostrarArray($numeros);
+echo '<hr/>';
+
+//contar número de elementos
+echo 'Número de elementos del array <br/>';
+echo count($numeros);
+
+//Buscar dentro de un array
+echo '<hr/>';
+$busqueda=24;
+echo 'Buscar dentro del array el número '.$busqueda.'<br/>';
+$search=array_search($busqueda, $numeros);
+//var_dump($search);
+if(!empty($search)){
+    echo '<h4>El número '.$busqueda.' existe en el array, en el índice '.$search.'</h4>';
+}else{
+    echo '<h4>El número '.$busqueda.' no existe en el array</h4>';
+}
