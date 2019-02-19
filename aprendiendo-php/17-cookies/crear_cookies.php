c355322c1b48bae075538d37791569360ac4d12c
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-02-19 18:25:25 -0400

    Avances_(18-2-19)

---
+++ aprendiendo-php/17-cookies/crear_cookies.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * Cookie: Es un fichero que se almacena en el ordenador del usuario que visita
+ * la web, con el fin de recordar datos o rastrear el comportamiento del mismo en la web
+ */
+//Crear cookie
+//setcookie('nombre', 'valor que solo puede ser texto', caducidad, ruta, dominio);
+//cookie básica
+setcookie('micookie', 'valor de mi galleta');
+
+//Cookie con expiración
+setcookie('oneyear', 'valor de mi cookie de 365 días', time()+(60*60*24*365));
+header('Location:ver_cookies.php');
+?>
---
+++ aprendiendo-php/17-cookies/ver_cookies.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+ * Para mostrar el valor de las cookies, tengo que usar $_COOKIE, una variable superglobal,
+ * es una array asociativo.
+ */
+if(isset($_COOKIE['micookie'])){
+    echo '<h1>'.$_COOKIE['micookie'].'</h1>';
+} else {
+    echo 'No existe la cookie<br/>';
+}
+
+if(isset($_COOKIE['oneyear'])){
+    echo '<h1>'.$_COOKIE['oneyear'].'</h1>';
+} else {
+    echo 'No existe la cookie<br/>';
+}
+?>
+<a href="crear_cookies.php">Crear mis galletas</a>
+<br/>
+<a href="borrar_cookies.php">Borrar mis galletas</a>
