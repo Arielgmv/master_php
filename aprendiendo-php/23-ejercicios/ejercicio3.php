6e6eb415f81e10113b055a305f1a1d5a5aca3b95
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-03-21 14:16:32 -0400

    Avances_(21-3-19)-1

--- aprendiendo-php/23-ejercicios/ejercicio3.php
+++ aprendiendo-php/23-ejercicios/ejercicio3.php
@@ -1,9 +1,23 @@
 <?php
-
 /* 
  * Ejercicio 3. Hacer una interfaz de usuario (formulario) con dos inputs y 4 botones
  * uno para sumar, restar, multiplicar y dividir.
  */
+$resultado = false;
+if(isset($_POST['n1']) && isset($_POST['n2'])){
+   if(isset($_POST['sumar'])){
+       $resultado='El resultado es: '.($_POST['n1'] + $_POST['n2']);
+   }
+   if(isset($_POST['restar'])){
+       $resultado='El resultado es: '.($_POST['n1'] - $_POST['n2']);
+   }
+   if(isset($_POST['multiplicar'])){
+       $resultado='El resultado es: '.($_POST['n1'] * $_POST['n2']);
+   }
+   if(isset($_POST['dividir'])){
+       $resultado='El resultado es: '.($_POST['n1'] / $_POST['n2']);
+   }
+}
 ?>
 <!DOCTYPE HTML>
 <html lang="es">
@@ -23,6 +37,11 @@
             <input type="submit" value="Multiplicar" name="multiplicar" />
             <input type="submit" value="Dividir" name="dividir" />
         </form>
-        <?php ?>
+        <?php
+            //Resultado
+            if($resultado!=false):
+                echo '<h2>'.$resultado.'</h2>';
+            endif;
+        ?>
     </body>
 </html>
