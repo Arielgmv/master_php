a8caa8e4f1dc29b5881327d28e934bc63c3279af
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-04-21 19:40:04 -0400

    Avances_(21-4-19)

--- aprendiendo-sql/05-agrupamiento/insert.sql
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -1,4 +1,17 @@
 /*Inserts para categorías*/
-INSERT INTO categorías
+INSERT INTO categorias VALUES(null, 'Acción');
+INSERT INTO categorias VALUES(null, 'Rol');
+INSERT INTO categorias VALUES(null, 'Deportes');
 
 /*Inserts para entradas*/
+INSERT INTO entradas VALUES(null, 1, 1, 'Novedades de GTA 5 Online', 'Review de GTA 5', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 2, 'Review de LOL Online', 'Todo sobre LOL', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 3, 'Nuevos jugadores de FIFA 2019', 'Review de FIFA 2019', CURDATE());
+
+INSERT INTO entradas VALUES(null, 2, 1, 'Novedades de Assasins', 'Review de Assasins', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 2, 'Review de WOW Online', 'Todo sobre WOW', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 3, 'Nuevos jugadores de PES 2019', 'Review de PES 2019', CURDATE());
+
+INSERT INTO entradas VALUES(null, 3, 1, 'Novedades de Call od Duty Online', 'Review de Call od Duty', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 2, 'Review de Fortnite Online', 'Todo sobre Fortnite', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 3, 'Nuevos jugadores de Formula 1 2020', 'Review de Formula 1 2020', CURDATE());
