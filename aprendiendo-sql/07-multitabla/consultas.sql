782fed687808942fa876efb59f78b22df77a938a
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-04-28 18:27:40 -0400

    Avances_(28-4-19)

--- aprendiendo-sql/07-multitabla/consultas.sql
+++ aprendiendo-sql/07-multitabla/consultas.sql
@@ -4,6 +4,13 @@ Son consultas que sirven para consultar varias tablas en una sola sentencia
 */
 /*Mostrar las entradas con el nombre del autor y el nombre de la categoría*/
 /*no me está funcionando*/
-SELECT e.titulo, u.nombre, c.nombre
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoría'
 FROM entradas e, usuarios u, categorias c
-WHERE e.usuario_id=u.id AND e.categoría_id=c.id;
+WHERE e.usuario_id=u.id AND e.categoria_id=c.id;
+
+/*Mostrar el nombre de las categorías y al lado cuantas entradas tienen*/
+SELECT c.nombre AS 'Categoría', COUNT(e.id) AS 'Cantidad de entradas'
+FROM categorias c, entradas e
+WHERE e.categoria_id=c.id GROUP BY e.categoria_id;
+
+/*Mostrar el email de los usuarios y al lado cuantas entradas tienen*/
