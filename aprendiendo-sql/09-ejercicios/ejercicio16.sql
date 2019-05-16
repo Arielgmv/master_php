5935b46b2a5d8482a9670db1aed8afe9ab8435ef
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-05-15 21:19:27 -0400

    Avances_(15-5-19)_1

--- aprendiendo-sql/09-ejercicios/ejercicio16.sql
+++ aprendiendo-sql/09-ejercicios/ejercicio16.sql
@@ -1,3 +1,15 @@
 /*
 16. Obtener un listado de clientes atendidos por el vendedor 'David Lopez'
 */
+SELECT nombre, vendedor_id FROM clientes WHERE vendedor_id=1;
+
+SELECT c.nombre, v.nombre FROM clientes c
+INNER JOIN vendedores v ON c.vendedor_id=v.id
+WHERE vendedor_id=1;
+
+UPDATE clientes SET vendedor_id=4 WHERE id=3;
+UPDATE clientes SET vendedor_id=4 WHERE id=5;
+UPDATE clientes SET vendedor_id=2 WHERE id=6;
+
+SELECT * FROM clientes WHERE vendedor_id IN
+(SELECT id FROM vendedores WHERE nombre = 'David' AND apellidos = 'Lopez');
