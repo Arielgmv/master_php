b61690d507c48a781838ae78edf6631c79599752
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-05-24 18:29:56 -0400

    Avances_(24-5-19)

--- aprendiendo-sql/09-ejercicios/ejercicio18.sql
+++ aprendiendo-sql/09-ejercicios/ejercicio18.sql
@@ -3,4 +3,6 @@
 */
 SELECT cliente_id FROM encargos WHERE coche_id = 5;
 
-SELECT cliente_id FROM encargos WHERE coche_id = 5;
+SELECT * FROM clientes WHERE id IN
+(SELECT cliente_id FROM encargos WHERE coche_id
+IN (SELECT id FROM coches WHERE modelo like '%Rav 4%'));
