cbb445ef8164f953e6b0032b2fdc53f3d65f38f0
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-08-06 09:46:15 -0400

    Avances_(6-8-19)

--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -1,16 +1,19 @@
 <?php
 //Librería con las funciones PHP
 //le estoy pasando el array de errores y le paso el campo (yo le estoy pasando - string)
+//necesitamos pasarle dos parametros $errores y $campo
 function mostrarError($errores, $campo){
     //por defecto alerta irá vacia
     $alerta='';
     if(isset($errores[$campo]) && !empty($campo)){
+        //voy a mostrar el error accediendo a la posicion del array en concreto que quiero
         $alerta="<div class='alerta alerta-error'>".$errores[$campo].'</div>';
     }
     //en caso de que haya un error
     return $alerta;
 }
 
+//borramos los errores una vez que terminanos de mostrarlos por primera vez
 function borrarErrores(){    
     $borrado=false;
     
--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -15,9 +15,9 @@
     </div>
 
     <div id="register" class="bloque">
-        <?php if(isset($_SESSION['errores'])): ?>
+        <?php /*if(isset($_SESSION['errores'])): ?>
             <?php var_dump($_SESSION['errores']); ?>
-        <?php endif; ?>
+        <?php endif; */?>
         <h3>Registrate</h3>
         <!--Mostrar errores-->
         <?php /*if(isset($_SESSION['completado'])): ?>
@@ -32,22 +32,23 @@
             <form action="registro.php" method="POST">
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre"/>
-                <?php /*echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : '';*/?>
-                        
+                <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : '';?>
+                <?php /*echo mostrarError($_SESSION['errores'], 'nombre');*/?>
+                
                 <label for="apellidos">Apellidos</label>
                 <input type="text" name="apellidos"/>
-                <?php /*echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos') : '';*/?>
+                <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos') : '';?>
 
                 <label for="email">Email</label>
                 <input type="email" name="email"/>
-                <?php /*echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : '';*/?>
+                <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : '';?>
 
                 <label for="password">Contraseña</label>
                 <input type="password" name="password"/>
-                <?php /*echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password') : '';*/?>
+                <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password') : '';?>
 
                 <input type="submit" name="submit" value="Registrar" />
             </form>
-            <?php /*borrarErrores();*/?>
+            <?php borrarErrores();?>
     </div>
 </aside>
