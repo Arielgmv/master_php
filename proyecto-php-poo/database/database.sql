2c0effab96116a56287efe301c65a82fce243bac
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-10-14 20:42:45 -0400

    Avances_(14-10-19)

--- proyecto-php-poo/database/database.sql
+++ proyecto-php-poo/database/database.sql
@@ -37,3 +37,27 @@ imagen          varchar (255),
 CONSTRAINT pk_productos PRIMARY KEY(id),
 CONSTRAINT fk_producto_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id)
 )ENGINE=InnoDb;
+
+CREATE TABLE pedidos(
+id          int (255) auto_increment not null,
+usuario_id  int (255) not null,
+provincia   varchar (100) not null,
+localidad   varchar (100) not null,
+direccion   varchar (255) not null,
+coste       float (200, 2) not null,
+estado      varchar (20) not null,
+fecha       date,
+hora        time,
+CONSTRAINT pk_pedidos PRIMARY KEY(id),
+CONSTRAINT fk_pedido_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE lineas_pedidos(
+id          int (255) auto_increment not null,
+pedido_id   int (255) not null,
+producto_id int (255) not null,
+unidades    int (255) not null,
+CONSTRAINT pk_lineas_pedidos PRIMARY KEY(id),
+CONSTRAINT fk_linea_pedido FOREIGN KEY(pedido_id) REFERENCES pedidos(id),
+CONSTRAINT fk_linea_producto FOREIGN KEY(producto_id) REFERENCES productos(id)
+)ENGINE=InnoDb;
