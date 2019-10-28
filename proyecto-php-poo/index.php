7486acfa2cc5d56ac96418cd70fe24f2781afe43
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-10-28 17:25:25 -0400

    Avances_(28-10-19)

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -43,6 +43,7 @@
                 <!--Barra Lateral-->
                 <aside id="lateral">
                     <div id="login" class="block_aside">
+                        <h3>Entrar a la web</h3>
                         <form action="#" method="post">
                             <label for="email">Email</label>
                             <input type="email" name="email" />
