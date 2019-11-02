bd35e545abfa5ad9c1f9c2b571f3fd38f7c3826c
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-11-02 16:30:24 -0400

    Avances_(2-11-19)

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,94 +1,47 @@
-<!DOCTYPE HTML>
-<html lang="es">
-    <head>
-        <meta charset="utf-8" />
-        <title>Tienda de camisetas</title>
-        <link rel="stylesheet" href="assets/css/style.css" />
-    </head>
-    <body>
-        <div id="container">
-            <!--Cabecera-->
-            <header id="header">
-                <div id="logo">
-                    <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
-                    <a href="index.php">
-                        Tienda de camisetas
-                    </a>
-                </div>
-            </header>
-            <!--Menu-->
-            <nav id="menu">
-                <ul>
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="#">Categoria 1</a>
-                    </li>    
-                    <li>
-                        <a href="#">Categoria 2</a>
-                    </li>    
-                    <li>
-                        <a href="#">Categoria 3</a>
-                    </li>    
-                    <li>
-                        <a href="#">Categoria 4</a>
-                    </li>    
-                    <li>
-                        <a href="#">Categoria 5</a>
-                    </li>
-                </ul>
-            </nav>
-            <div id="content">
-                <!--Barra Lateral-->
-                <aside id="lateral">
-                    <div id="login" class="block_aside">
-                        <h3>Entrar a la web</h3>
-                        <form action="#" method="post">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" />
-                            <label for="password">Contraseña</label>
-                            <input type="password" name="password" />
-                            <input type="submit" value="Enviar" />
-                        </form>
+<h1>Bienvenido a mi web con MVC</h1>
+<?php
 
-                        <ul>
-                            <li><a href="#">Mis pedidos</a></li>
-                            <li><a href="#">Gestionar pedidos</a></li>
-                            <li><a href="#">Gestionar categorias</a></li>
-                        </ul>
-                    </div>
-                </aside>    
-                
-                <!--Contenido Central-->
-                <div id="central">
-                    <h1>Productos destacados</h1>
-                    <div class="product">
-                        <img src="assets/img/camiseta.png" />
-                        <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="" class="button">Comprar</a>
-                    </div>
+//use Misclases\Usuario;
 
-                    <div class="product">
-                        <img src="assets/img/camiseta.png" />
-                        <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="" class="button">Comprar</a>
-                    </div>
+//mostrar errores en PHP: https://stackify.com/display-php-errors/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-                    <div class="product">
-                        <img src="assets/img/camiseta.png" />
-                        <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="" class="button">Comprar</a>
-                    </div>
-                </div>
-            </div>
-            <!--Pie de página-->
-            <footer id="footer">
-                <p>Desarrollado por Ariel Muñoz &copy; <?=date('Y')?></p>
-            </footer>
-        </div>
-    </body>
-</html>
+require_once 'autoload.php';
+
+if (isset($_GET['controller'])) {
+    $nombre_controlador = $_GET['controller'].'Controller';
+    //var_dump($nombre_controlador);
+}else {
+    echo "La pagina que buscas no existe 1";
+    exit();
+}
+
+if (class_exists($nombre_controlador)) {
+    
+    $controlador = new $nombre_controlador();
+
+    //$controlador->MostrarTodos();
+    //$controlador->crear();
+    //$_GET['action'] es variable
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+        //se llama con http://127.0.0.1/master_php/aprendiendo-php-mvc/?action=MostrarTodos
+        //http://127.0.0.1/master_php/aprendiendo-php-mvc/?action=crear
+        //haciendo lo siguiente invoca el metodo que tenemos en nuestra clase
+        $action = $_GET['action'];
+        $controlador->$action();
+    }else {
+        echo "La pagina que buscas no existe 2";
+    }
+}else {
+    echo "La pagina que buscas no existe 3";
+}
+
+//llamamos con http://127.0.0.1/master_php/aprendiendo-php-mvc/?controller=UsuarioController&action=crear
+//http://127.0.0.1/master_php/aprendiendo-php-mvc/?controller=UsuarioController&action=mostrarTodos
+//http://127.0.0.1/master_php/aprendiendo-php-mvc/?=UsuarioController&action=mostrarTodos
+
+//http://127.0.0.1/master_php/aprendiendo-php-mvc/?controller=Usuario&action=crear
+//http://127.0.0.1/master_php/aprendiendo-php-mvc/?controller=Nota&action=listar
+
