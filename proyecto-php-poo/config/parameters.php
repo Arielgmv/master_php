8383c4f64c618ad55411fe136a56a91d76810dc6
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-11-06 17:06:56 -0400

    Avances_(6-11-19)

--- proyecto-php-poo/config/parameters.php
+++ proyecto-php-poo/config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
-define("base_url", "http://127.0.0.1/master_php/proyecto-php-poo/");
+define("base_url", "http://localhost/master_php/proyecto-php-poo/");
 define("controller_default", "productoController");
 define("action_default", "index");
--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,22 +1,28 @@
 <?php
 //mostrar errores en PHP: https://stackify.com/display-php-errors/
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 
 require_once 'autoload.php';
 require_once 'config/parameters.php';
 require_once 'views/layout/header.php';
 require_once 'views/layout/sidebar.php';
+require_once 'controllers/ErrorController.php';
 
+function show_error(){
+    $error = new errorController();    
+    $error->index();
+}
 
 if (isset($_GET['controller'])) {
     $nombre_controlador = $_GET['controller'].'Controller';
     //var_dump($nombre_controlador);
-}else {
-    $error = new errorController();    
-    $error->index();
-    echo "La pagina que buscas no existe 1";
+}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+    $nombre_controlador = controller_default;
+}else{
+    show_error();
+    //echo "La pagina que buscas no existe 1";
     exit();
 }
 
@@ -29,15 +35,17 @@ if (class_exists($nombre_controlador)) {
         //haciendo lo siguiente invoca el metodo que tenemos en nuestra clase
         $action = $_GET['action'];
         $controlador->$action();
-    }else {
-        $error = new errorController();
-        $error->index();
-        echo "La pagina que buscas no existe 2";
+    }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $action_default = action_default;
+        $controlador->$action_default();
+    }
+    else {
+        show_error();
+        //echo "La pagina que buscas no existe 2";
     }
 }else {
-    $error = new errorController();    
-    $error->index();
-    echo "La pagina que buscas no existe 3";
+    show_error();
+    //echo "La pagina que buscas no existe 3";
 }
 
 require_once 'views/layout/footer.php';
