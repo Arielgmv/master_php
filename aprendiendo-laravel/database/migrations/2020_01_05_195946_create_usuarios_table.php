ed260e99775a95b33b412fb7230eb204462eb344
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-01-05 16:21:55 -0400

    Avances_(5-1-19)

---
+++ aprendiendo-laravel/database/migrations/2020_01_05_195946_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //evento up que crea la tabla
+    public function up()    
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->integer('edad');
+            $table->float('sueldo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //evento down que borra la tabla
+    public function down()
+    {
+        Schema::drop('usuarios');
+    }
+}
