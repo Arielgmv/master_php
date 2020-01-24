6ec158b9a7310b5b1e61ec0ea3a4471039739f2b
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-01-23 21:15:32 -0400

    Avance_(23-1-20)

--- angular/my-first-project/src/app/cliente/carrito/carrito.component.ts
+++ angular/my-first-project/src/app/cliente/carrito/carrito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
@@ -11,6 +12,17 @@ export class CarritoComponent implements OnInit {
   estado:boolean;
   cant_productos:number = 0;
   categorias:any = [];
+  productos:any = [];
+  cant_venta:any= 1;
+  
+  carrito:any = [];
+  total_pagar:number = 0;
+
+  productoForm = new FormGroup({
+    nombre: new FormControl(''),
+    precio: new FormControl(''),
+    cantidad: new FormControl('') 
+  })
 
   constructor() { 
     this.titulo = "Carrito de compras 1";
@@ -19,6 +31,23 @@ export class CarritoComponent implements OnInit {
     this.categorias.push("ropa");
     this.categorias.push("muebles");
     this.categorias.push("juguetes");
+
+    this.productos.push({
+                        nombre:"chamarra",
+                        precio: 250,
+                        cantidad: 12
+                      });
+    this.productos.push({
+                        nombre:"mesa",
+                        precio: 550,
+                        cantidad: 6
+                      });
+    this.productos.push({
+                        nombre:"zapato",
+                        precio: 250,
+                        cantidad: 18
+                      });                  
+
   }
 
   ngOnInit() {
@@ -32,4 +61,33 @@ export class CarritoComponent implements OnInit {
     }
   }
 
+  eliminar_producto(i){
+    this.productos.splice(i, 1);
+  }
+
+  agregar_carrito(prod){
+    this.carrito.push({
+      nombre: prod.nombre,
+      cantidad: this.cant_venta,
+      precio: prod.precio
+    })
+    this.cant_venta = 1;
+
+    this.calcular_total_pagar();
+  }
+
+  cambio(data){
+    this.cant_venta = data.target.value;
+  }
+
+  calcular_total_pagar(){
+    this.total_pagar = 0;
+    for(let i=0; i<this.carrito.length; i++){
+      this.total_pagar += this.carrito[i].precio*this.carrito[i].cantidad;
+    }
+  }
+
+  agregar_producto(){
+    this.productos.push(this.productoForm.value);
+  }
 }
