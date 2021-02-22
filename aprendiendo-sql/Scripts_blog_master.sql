eb7e92fe2dacf1a044f6c8111960a906fa12a004
Arielgmv <ariel.munoz.villegas@gmail.com>
2021-02-22 15:34:09 -0400

    Avances_(22-2-21)

--- aprendiendo-sql/Scripts_blog_master.sql
+++ aprendiendo-sql/Scripts_blog_master.sql
@@ -54,4 +54,40 @@ SELECT USER() FROM usuarios;
 #si es nulo#
 SELECT IFNULL(apellidos, 'Campo vacio') FROM usuarios;
 
+/*Modificar filas / actualizar datos*/
+UPDATE blog_master.usuarios set usuarios.fecha=CURDATE(), usuarios.nombre='admin' WHERE usuarios.id=5;
+
+
+/*Consulta con una condición*/
+SELECT * from blog_master.usuarios as usu where usu.email LIKE 'ariel%';
+
+/*Ejemplos*/
+/* 1. Mostrar nombre y apellidos de todos los usuarios registrados en 2019*/
+SELECT u.nombre, u.apellidos, u.fecha FROM blog_master.usuarios as u WHERE YEAR(u.fecha)=2019;
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha)=2019;
+
+/*2. Mostrar nombre y apellidos de todos los usuarios que no se registraron en 2019*/
+SELECT u.nombre, u.apellidos, u.fecha FROM blog_master.usuarios u WHERE YEAR(u.fecha)!=2019; 
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) != 2019;
+
+/*3. Mostrar el email de los usuarios cuyo apellido incluya la letra a y que contraseña sea 12345678*/
+SELECT u.email, u.apellidos FROM blog_master.usuarios u WHERE u.apellidos LIKE '%a%' AND u.password = '12345678';
+SELECT email FROM usuarios WHERE apellidos LIKE '%a%' and password='12345678';
+
+/*4. Sacar todos los registros de la tabla usuarios cuyo año sea par*/
+SELECT * FROM blog_master.usuarios u WHERE YEAR(u.fecha)%2=0; 
+SELECT * FROM usuarios WHERE (YEAR(fecha)%2=0);
+
+/*5. Sacar todos los registros de la tabla usuarios cuyo nombre tenga más de 4 letras y que se hayan registrado
+antes de 2020 y mostrar en mayúsculas*/
+SELECT UPPER(u.nombre) as Nombre, u.apellidos, LENGTH(u.nombre) FROM blog_master.usuarios u WHERE LENGTH(u.nombre)>4 AND YEAR((u.fecha))<2020;
+SELECT UPPER(nombre) AS 'Nombre', apellidos FROM usuarios WHERE (LENGTH(nombre)>4) AND (YEAR(fecha))<2020;
+
+/*Order by*/
+SELECT * FROM blog_master.usuarios u ORDER BY u.id;
+SELECT * FROM blog_master.usuarios u ORDER BY u.id DESC;
+
+/*Limit*/
+SELECT * FROM blog_master.usuarios u LIMIT 1;
+SELECT * FROM blog_master.usuarios u LIMIT 1, 2;
 
