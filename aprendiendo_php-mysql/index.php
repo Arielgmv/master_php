1f72cb2288ef54ed1d62acd78e4b3b6136b645dc
Arielgmv <ariel.munoz.villegas@gmail.com>
2021-03-01 12:26:22 -0400

    Avances_(1-3-21)

--- aprendiendo_php-mysql/index.php
+++ aprendiendo_php-mysql/index.php
@@ -1,6 +1,7 @@
 <?php
 //Conectar a la base de datos
-$conexion=mysqli_connect('localhost', 'root', '12345678', 'phpmysql');
+//$conexion=mysqli_connect('localhost', 'root', '12345678', 'phpmysql');
+$conexion=mysqli_connect('localhost', 'root', '12345678', 'blog_master');
 
 //Comprobar la conexion
 if(mysqli_connect_errno()){
@@ -13,21 +14,37 @@ echo '<br>';
 mysqli_query($conexion, "SET NAMES 'utf8'");
 
 //Consulta SELECT desde PHP
-$query=mysqli_query($conexion, "SELECT * FROM notas");
+//$query=mysqli_query($conexion, "SELECT * FROM notas");
+$query=mysqli_query($conexion, "SELECT * FROM blog_master.usuarios");
 
-while($nota=mysqli_fetch_assoc($query)){
+/*while($nota=mysqli_fetch_assoc($query)){
     //var_dump($nota);
     echo '<h2>'.$nota['titulo'].'</h2>';
     echo $nota['descripcion'].'<br>';
+}*/
+while($usuarios=mysqli_fetch_assoc($query)){
+    /*echo ('<pre>');
+    var_dump($usuarios);
+    echo ('</pre>');*/
+    echo '<h2>'.$usuarios['nombre'].'</h2>';
+    echo $usuarios['email'].'<br>';
 }
-
 //Insertar en la base de datos desde PHP
-$sql="INSERT INTO notas VALUES (null, 'Nota desde PHP', 'Esto es una nota de PHP', 'green')";
+/*$sql="INSERT INTO notas VALUES (null, 'Nota desde PHP', 'Esto es una nota de PHP', 'green')";
 $insert=mysqli_query($conexion, $sql);
 
 if($insert){
     echo 'Datos insertados correctamente';
 }else{
+    echo 'Error: '.mysqli_error($conexion);
+}*/
+
+$sql="INSERT INTO blog_master.usuarios VALUES (null, 'Nombre desde PHP', 'Apellido desde PHP', 'php@php.com', 'password', CURDATE())";
+$insert=mysqli_query($conexion, $sql);
+
+if ($insert) {
+    echo 'Datos ingresados correctamente';
+} else {
     echo 'Error: '.mysqli_error($conexion);
 }
 
---
+++ proyecto-php-1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8"><!--esto permite meter ñ, tildes y símbolos raros-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de Videojuegos</title>
+</head>
+<body>
+    <!--Cabecera-->
+    <header></header>
+    <!--Menu-->
+    <nav></nav>
+    <!--Barra Lateral-->
+    <aside></aside>
+    <!--Caja Principal, section / article-->
+    <section></section>
+    <!--Pie de Página-->
+    <footer></footer>
+</body>
+</html>
