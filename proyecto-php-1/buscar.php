6b25de78b785e423b024488fb9a7aedf850f4cb5
Arielgmv <ariel.munoz.villegas@gmail.com>
2021-04-21 19:27:52 -0400

    Avances_(21-4-21)

---
+++ proyecto-php-1/buscar.php
@@ -0,0 +1,57 @@
+<?php require_once './includes/conexion.php'; ?>
+<?php require_once './includes/helpers.php'; ?>
+<?php
+    if (!isset($_POST['busqueda'])) {
+        header("Location: index.php");
+    }
+    $busqueda = conseguirEntradas($db, null, null, $_POST['busqueda']);
+    /*var_dump($categoria);
+    die();*/
+    if (!isset($categoria_actual['id'])) {
+        header("Location: index.php");
+    }
+?>
+
+<?php require_once './includes/cabecera.php'; ?>
+<?php require_once './includes/lateral.php'; ?>
+
+<!--Caja Principal, section / article-->
+<div id="principal">    
+    <?php
+    //var_dump($categoria_actual);    
+    ?>
+    <h1>Entradas de <?=$categoria_actual['nombre']?></h1>
+    
+    <?php
+        $entradas = conseguirEntradas($db, null, $_GET['id']);
+        /*echo ('<pre>');
+        var_dump($_GET['id']);
+        var_dump($entradas); 
+        echo ('</pre>');*/
+        if (!empty($entradas) && mysqli_num_rows($entradas) >=1) :
+            while ($entrada = mysqli_fetch_assoc($entradas)) :
+    ?>
+            <article class="entrada">
+                <?php 
+                /*echo ('<pre>');
+                var_dump($entrada); 
+                echo ('</pre>');*/
+                ?>                
+                <a href="entrada.php?id=<?=$entrada['id']?>">
+                    <h2><?=$entrada['titulo']?></h2>
+                        <span class="fecha"><?=$entrada['categoria'].' | '.$entrada['fecha']?></span>
+                        <p>
+                            <?=substr($entrada['descripcion'], 0, 180)."..." ?>
+                        </p>
+                </a>
+            </article>
+    <?php            
+            endwhile;
+        else:
+    ?>
+    <div class="alerta">No hay entradas en esta categoría</div>
+    <?php endif; ?>
+</div>
+<!--Fin principal-->
+
+<?php require_once './includes/pie.php'; ?>
